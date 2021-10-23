3bb7b0259f7e0d6b7b76ee3c21f6f0b1d86c0a13
Александр Фирсов <firsovalex@yandex.ru>
2021-10-23 17:23:19 +0300

    AddTestmodul

--- Converter/ConverterForm.cs
+++ Converter/ConverterForm.cs
@@ -6,7 +6,7 @@ namespace Converter
 {
     public partial class ConverterForm : Form
     {
-        private int number = 0;
+        StringBuilder sbstr = new StringBuilder();
 
         public ConverterForm()
         {
@@ -18,26 +18,71 @@ namespace Converter
             if (textBox_Decimal.Text != "")
             {
                 //Проверка
-                //number = Convert.ToInt32(textBox_Decimal.Text);
                 //textBox_Binary.Text = Convert.ToString(number, 2);
-                //textBox_Hexadecimal.Text = Convert.ToString(number, 16);
+                //textBox_Ternary.Text = ToTrenary(Convert.ToInt32(textBox_Decimal.Text));
                 //textBox_Octal.Text = Convert.ToString(number, 8);
-
-                textBox_Ternary.Text = ToTrenary(number);
+                //textBox_Hexadecimal.Text = Convert.ToString(number, 16);
+                textBox_Binary.Text = ToBinary(Convert.ToInt32(textBox_Decimal.Text));
+                textBox_Ternary.Text = ToTernary(Convert.ToInt32(textBox_Decimal.Text));
+                textBox_Octal.Text = ToOctal(Convert.ToInt32(textBox_Decimal.Text));
             }
         }
 
-        public String ToTrenary(int value)
+        /// <summary>
+        /// Перевод из 10-ой системы счисления в 2-ю
+        /// </summary>
+        /// <remarks>Примечание:</remarks>
+        /// <param name="value">10-ое число</param>
+        /// <returns>Двоичное представление числа как строку</returns>
+        public string ToBinary(int value)
         {
-            StringBuilder Sb = new StringBuilder();
+            sbstr.Clear();//Очитска подстроки
 
-            while (value > 0)
+            while (value > 0)//Цикл перевода
             {
-                Sb.Insert(0, value % 3);
+                sbstr.Insert(0, value % 2);//2 индекс перевода/делитель
+                value /= 2;
+            }
+
+            return sbstr.ToString();//Вывод
+        }
+
+        /// <summary>
+        /// Перевод из 10-ой системы счисления в 3-ю
+        /// </summary>
+        /// <remarks>Примечание:</remarks>
+        /// <param name="value">10-ое число</param>
+        /// <returns>Троичиное представление числа как строку</returns>
+        public string ToTernary(int value)
+        {
+            sbstr.Clear();//Очитска подстроки
+
+            while (value > 0)//Цикл перевода
+            {
+                sbstr.Insert(0, value % 3);//3 индекс перевода/делитель
                 value /= 3;
             }
 
-            return Sb.ToString();
+            return sbstr.ToString();//Вывод
+        }
+
+        /// <summary>
+        /// Перевод из 10-ой системы счисления в 8-ю
+        /// </summary>
+        /// <remarks>Примечание:</remarks>
+        /// <param name="value">10-ое число</param>
+        /// <returns>Восмеричное представление числа как строку</returns>
+        public string ToOctal(int value)
+        {
+            sbstr.Clear();//Очитска подстроки
+
+            while (value > 0)//Цикл перевода
+            {
+                sbstr.Insert(0, value % 8);//8 индекс перевода/делитель
+                value /= 8;
+            }
+
+            return sbstr.ToString();//Вывод
         }
     }
 }
